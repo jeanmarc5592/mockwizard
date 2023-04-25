42d98095d26847cdd28445ee7603663cd5b0288c
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-04-25 17:44:42 +0200

    Add UUID module to MockWizard

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
-import { AbstractPerson } from "./modules/Person/AbstractPerson";
+import { AbstractPerson } from "./modules/Person";
 import { Factory as PersonFactory } from "./modules/Person/Factory";
 import { Locale } from "./modules/Person/types";
 
+import { UUID } from "./modules/UUID";
+
 interface MockWizardInterface {
   person: AbstractPerson;
+  uuid: UUID;
 }
 
 export class MockWizard implements MockWizardInterface {
   public person: AbstractPerson;
 
+  public uuid: UUID;
+
   constructor(locale: Locale = "en-US") {
     this.person = PersonFactory.create(locale);
+    this.uuid = new UUID();
   }
 }
