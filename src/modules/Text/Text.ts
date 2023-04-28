6ed6534e03c25889e64651fd81f411c059b48d8b
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-04-28 16:30:58 +0200

    Add "word" and "words" methods to Text Module

---
+++ src/modules/Text/Text.ts
@@ -0,0 +1,41 @@
+import { TextData } from "./data";
+import { RandomGenerator } from "../Utils";
+import { TextOptions } from "./types";
+
+export class Text {
+  private readonly data: string[];
+
+  constructor() {
+    this.data = TextData.words;
+  }
+
+  /**
+   * Generates a random word.
+   *
+   * @public
+   * @returns {string} A random word from the data provided.
+   */
+  public word(): string {
+    return RandomGenerator.generateValue(this.data) as string;
+  }
+
+  /**
+   * Generates an array of random words.
+   *
+   * @public
+   * @param {TextOptions} [options={}] - An optional object containing additional options.
+   * @param {number} [options.amount] - The number of random words to generate. If not specified, it defaults to 3.
+   * @param {boolean} [options.asString=false] - If true, the generated words will be returned as a single string.
+   * @returns {(string|string[])} An array of random words, or a single string if 'asString' option is true.
+   */
+  public words(options: TextOptions = {}): string | string[] {
+    const amount = options.amount ?? 3;
+    const words = RandomGenerator.generateMultipleValues(this.data, { amount }) as string[];
+
+    if (options.asString) {
+      return words.join(" ");
+    }
+
+    return words;
+  }
+}
---
+++ src/modules/Text/types.ts
@@ -0,0 +1,4 @@
+export interface TextOptions {
+  amount?: number;
+  asString?: boolean;
+}
