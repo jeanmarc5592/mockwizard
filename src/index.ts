293b5c613efceabc57101d557a21a2b37b96c45c
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-04-28 16:32:09 +0200

    Add Text module to MockWizard Facade

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,12 @@ import { Locale } from "./modules/Person/types";
 
 import { UUID } from "./modules/UUID";
 
+import { Text } from "./modules/Text";
+
 interface MockWizardInterface {
   person: AbstractPerson;
   uuid: UUID;
+  text: Text;
 }
 
 export class MockWizard implements MockWizardInterface {
@@ -22,12 +25,21 @@ export class MockWizard implements MockWizardInterface {
    * The property that holds the UUID module
    *
    * @public
-   * @type {AbstractPerson}
+   * @type {UUID}
    */
   public readonly uuid: UUID;
+
+  /**
+   * The property that holds the Text module
+   *
+   * @public
+   * @type {Text}
+   */
+  public readonly text: Text;
 
   constructor(locale: Locale = "en-US") {
     this.person = PersonFactory.create(locale);
     this.uuid = new UUID();
+    this.text = new Text();
   }
 }
