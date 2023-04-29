45613a8b28516a1196be64fe76858320a57190bf
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-04-29 14:17:04 +0200

    Write Unit Tests for "sentence" method

--- src/modules/Text/Text.test.ts
+++ src/modules/Text/Text.test.ts
@@ -20,7 +20,7 @@ describe("Text", () => {
   });
 
   describe("words", () => {
-    it("should generate 3 random words from the given list if 'amount' option is not specified", () => {
+    it("should generate 3 random words from the given list if 'words' option is not specified", () => {
       const results = textMock.words() as string[];
       const wordsList = Reflect.get(textMock, "data");
 
@@ -32,24 +32,20 @@ describe("Text", () => {
       expect(results).toHaveLength(3);
     });
 
-    it("should generate as many results as the 'amount' option is set to", () => {
+    it("should generate as many results as the 'words' option is set to", () => {
       const wordsList = Reflect.get(textMock, "data");
 
-      const amountOne = 5;
-      const amountTwo = 20;
-      const amountThree = 100;
+      const wordsCountOne = 5;
+      const wordsCountTwo = 20;
+      const wordsCountFour = 100;
 
-      const resultsOne = textMock.words({ amount: amountOne }) as string[];
-      const resultsTwo = textMock.words({ amount: amountTwo }) as string[];
-      const resultsThree = textMock.words({ amount: amountThree }) as string[];
+      const resultsOne = textMock.words({ words: wordsCountOne }) as string[];
+      const resultsTwo = textMock.words({ words: wordsCountTwo }) as string[];
+      const resultsThree = textMock.words({ words: wordsCountFour }) as string[];
 
-      expect(resultsOne).toBeInstanceOf(Array);
-      expect(resultsTwo).toBeInstanceOf(Array);
-      expect(resultsThree).toBeInstanceOf(Array);
-
-      expect(resultsOne).toHaveLength(amountOne);
-      expect(resultsTwo).toHaveLength(amountTwo);
-      expect(resultsThree).toHaveLength(amountThree);
+      expect(resultsOne).toHaveLength(wordsCountOne);
+      expect(resultsTwo).toHaveLength(wordsCountTwo);
+      expect(resultsThree).toHaveLength(wordsCountFour);
 
       resultsOne.forEach(result => {
         expect(wordsList).toContain(result);
@@ -62,10 +58,59 @@ describe("Text", () => {
       });
     });
 
+    it("should return the results as an array of strings if 'asString' option is not specified", () => {
+      const result = textMock.words() as string[];
+
+      expect(result).toBeInstanceOf(Array);
+    });
+
     it("should return the results as a single string if 'asString' option is specified", () => {
       const result = textMock.words({ asString: true }) as string;
 
       expect(typeof result).toBe("string");
+    });
+  });
+
+  describe("sentence", () => {
+    it("should generate a sentence with 5 random words from a given list if 'words' option is not specified", () => {
+      const result = textMock.sentence() as string[];
+
+      expect(result).toHaveLength(5);
+    });
+
+    it("should generate a sentence with as many words as in the 'words' option is specified from a given list", () => {
+      const result = textMock.sentence({ words: 10 }) as string[];
+
+      expect(result).toHaveLength(10);
+    });
+
+    it("should return the sentence as an array if 'asString' option is not specified", () => {
+      const result = textMock.sentence() as string[];
+
+      expect(result).toBeInstanceOf(Array);
+    });
+
+    it("should return the sentence as a string if 'asString' option is specified", () => {
+      const result = textMock.sentence({ asString: true }) as string;
+
+      expect(typeof result).toBe("string");
+    });
+
+    it("should make the first character of the first word uppercase", () => {
+      const result = textMock.sentence() as string[];
+      
+      const firstChar = result[0].charAt(0);
+
+      expect(firstChar).toMatch((/[A-Z]/));
+    });
+
+    it("should add a dot after the last word", () => {
+      const result = textMock.sentence() as string[];
+
+      const lastWord = result[result.length - 1];
+      const lastChar = lastWord.charAt(lastWord.length - 1);
+
+      expect(lastChar).toBe(".");
     });
   });
 });
