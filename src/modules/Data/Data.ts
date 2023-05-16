fdd409783ac37d5febb765e5d86bed18a60dc839
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-05-16 15:17:13 +0200

    Implement "digit" method

--- src/modules/Data/Data.ts
+++ src/modules/Data/Data.ts
@@ -1,1 +1,18 @@
-export class Data {}
+import { RandomGenerator } from "../../utils";
+import { DigitOptions } from "./types";
+
+export class Data {
+  /**
+   * Generates a random digit between 0 and 9.
+   *
+   * @param {DigitOptions} [options] - Optional configuration for generating the random digit.
+   * @param {boolean} [options.notNull] - If true, the generated digit will not be 0.
+   * @returns {number} - The generated random digit.
+   */
+  digit(options: DigitOptions = {}): number {
+    const start = options.notNull ? 1 : 0;
+    const end = 9;
+
+    return RandomGenerator.generateNumberBetween(start, end);
+  }
+}
