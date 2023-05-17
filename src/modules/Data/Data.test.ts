09e271115f485bc01ee133e8d216e8d0b028581f
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-05-17 14:16:49 +0200

    Write Unit Tests for "float" method

--- src/modules/Data/Data.test.ts
+++ src/modules/Data/Data.test.ts
@@ -67,4 +67,64 @@ describe("Data", () => {
       expect(() => dataMock.integer({ digitsCount: true })).toThrowError(error);
     });
   });
+
+  describe("float", () => {
+    it("should create random floating number with 1-10 decimals and 1-10 integers", () => {
+      const float = dataMock.float();
+      
+      const integers = float.toString().split(".")[0].length;
+      const firstInteger = float.toString().charAt(0);
+      
+      const decimals = float.toString().split(".")[1].length;
+
+      expect(typeof float).toBe("number");
+      expect(Number.isInteger(float)).not.toBe(true);
+
+      expect(integers).toBeGreaterThanOrEqual(1);
+      expect(integers).toBeLessThanOrEqual(10);
+
+      expect(decimals).toBeGreaterThanOrEqual(1);
+      expect(decimals).toBeLessThanOrEqual(10);
+
+      expect(firstInteger).not.toBe("0");
+    });
+
+    it("should create a random floating number with as many decimals as specified in 'decimalsCount' option", () => {
+      const float = dataMock.float({ decimalsCount: 8 });
+      const decimals = float.toString().split(".")[1].length;
+
+      expect(typeof float).toBe("number");
+      expect(Number.isInteger(float)).not.toBe(true);
+      expect(decimals).toBe(8);
+    });
+
+    it("should create a random floating number with as many integers as specified in 'integersCount' option", () => {
+      const float = dataMock.float({ integersCount: 3 });
+      const integers = float.toString().split(".")[0].length;
+
+      expect(typeof float).toBe("number");
+      expect(Number.isInteger(float)).not.toBe(true);
+      expect(integers).toBe(3);
+    });
+
+    it("should throw an Error if 'decimalsCount' option is less than 1 or greater than 10", () => {
+      const error = "Option 'decimalsCount' has to be greater than 0 and less than or equal to 10.";
+
+      expect(() => dataMock.float({ decimalsCount: -4 })).toThrowError(error);
+      expect(() => dataMock.float({ decimalsCount: 234 })).toThrowError(error);
+    });
+
+    it("should throw an Error if 'decimalsCount' option is not a number", () => {
+      const error = "Option 'decimalsCount' has to be a number.";
+
+      // @ts-ignore
+      expect(() => dataMock.float({ decimalsCount: "4" })).toThrowError(error);
+
+      // @ts-ignore
+      expect(() => dataMock.float({ decimalsCount: ["4"] })).toThrowError(error);
+
+      // @ts-ignore
+      expect(() => dataMock.float({ decimalsCount: true })).toThrowError(error);
+    });
+  });
 });
