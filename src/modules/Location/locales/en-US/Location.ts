1a3a37cf0e1db6a99af8d2b4418be9fd74a2f63c
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-05-23 19:44:21 +0200

    Create new subclass "Location" for "en-US" locale

--- src/modules/Location/locales/en-US/Location.ts
+++ src/modules/Location/locales/en-US/Location.ts
@@ -1,3 +1,12 @@
 import { AbstractLocation } from "../../AbstractLocation";
+import { State } from "../../types";
+import { LocationData } from "./data";
 
-export class Location extends AbstractLocation {}
+export class Location extends AbstractLocation {
+  protected statesList: State[];
+
+  constructor() {
+    super();
+    this.statesList = LocationData.states;
+  }
+}
