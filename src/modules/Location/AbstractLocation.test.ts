1c01ddaa6a5636383fc0a45e6bacd79e2ca67010
Jean-Marc Möckel <jeanmarcmoeckel@gmail.com>
2023-05-23 20:39:53 +0200

    Write Unit Test for "city" Method

--- src/modules/Location/AbstractLocation.test.ts
+++ src/modules/Location/AbstractLocation.test.ts
@@ -1,9 +1,11 @@
 import "reflect-metadata";
 import { AbstractLocation } from "./AbstractLocation";
-import { State } from "./types";
+import { City, State } from "./types";
 
 class LocationMock extends AbstractLocation {
   protected statesList: State[];
+
+  protected citiesList: City[];
 
   constructor() {
     super();
@@ -15,6 +17,15 @@ class LocationMock extends AbstractLocation {
       { name: "Vermont", abbreviation: "VT" },
       { name: "Virginia", abbreviation: "VA" },
       { name: "Washington", abbreviation: "WA" },
+    ];
+    this.citiesList = [
+      { name: "Overland Park", state: "Kansas" },
+      { name: "Providence", state: "Rhode Island" },
+      { name: "Garden Grove", state: "California" },
+      { name: "Chattanooga", state: "Tennessee" },
+      { name: "Oceanside", state: "California" },
+      { name: "Jackson", state: "Mississippi" },
+      { name: "Fort Lauderdale", state: "Florida" },
     ];
   }
 }
@@ -29,8 +40,8 @@ describe("AbstractLocation", () => {
   describe("state", () => {
     it("should generate a random state name", () => {
       const state = locationMock.state();
-      const statesList = Reflect.get(locationMock, "statesList");
-      const stateNames = statesList.map((state: State) => state.name);
+      const statesList = Reflect.get(locationMock, "statesList") as State[];
+      const stateNames = statesList.map((state) => state.name);
 
       expect(typeof state).toBe("string");
       expect(stateNames).toContain(state);
@@ -43,6 +54,17 @@ describe("AbstractLocation", () => {
 
       expect(typeof state).toBe("string");
       expect(stateAbbreviations).toContain(state);
+    });
+  });
+
+  describe("city", () => {
+    it("should generate a random city name", () => {
+      const city = locationMock.city();
+      const citiesList = Reflect.get(locationMock, "citiesList") as City[];
+      const cityNames = citiesList.map(city => city.name);
+
+      expect(typeof city).toBe("string");
+      expect(cityNames).toContain(city);
     });
   });
 });
